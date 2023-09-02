62339a9c5ac84aa927c9d4e53182ab25d73668e4
drestrep <drestrepoag@unal.edu.co>
2023-09-01 20:37:27 -0500

    Changin' TS

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon=1):
+    def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
 
     def __str__(self):
-        return self._nombre + " remoto"
+        return f"{self._nombre} {self._salon}"
